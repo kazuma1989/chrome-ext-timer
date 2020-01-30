a5108081449904deebb16d669459e55798ffb97b
kazuma1989 <kazuma.ebina@quipper.com>
2020-01-30 18:54:08 +0900

    chore: Init Preact app

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { h, render } from 'preact'
+import { useState } from 'preact/hooks'
+
+function App() {
+  const [name, setName] = useState('')
+
+  return (
+    <div>
+      <h1>Hello {name?.toUpperCase() || 'World'}!</h1>
+      <input value={name} onInput={e => setName(e.currentTarget.value)} />
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root')!)
