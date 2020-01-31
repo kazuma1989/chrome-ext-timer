5d3ba27a86e60ac583ff4e568bcce6eafc415f91
kazuma1989 <kazuma.ebina@quipper.com>
2020-01-31 13:07:05 +0900

    refactor: Pack App into a shadow DOM

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import { h, render } from 'preact'
 import App from './App'
 
 const root = document.createElement('div')
-document.body.appendChild(root)
-
 render(<App />, root)
+
+const shadowContainer = document.createElement('div')
+document.body.appendChild(shadowContainer)
+
+const shadow = shadowContainer.attachShadow({ mode: 'closed' })
+shadow.innerHTML = `<style>:host { all: initial; }</style>`
+shadow.appendChild(root)
